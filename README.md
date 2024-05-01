9bc3b8f5efbac01a9db0afeb702ff9aca445ecb1
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-05-01 20:37:33 +0530

    User Login Code Added

---
+++ README.md
@@ -0,0 +1,15 @@
+Install this package first
+
+npm init -y        
+npm i express 
+npm i ejs   
+npm i mongoose     
+npm i bcrypt       
+npm i jsonwebtoken   
+npm i cookie-parser
+
+
+Run command
+npx nodemon .\app.js
+
+open mongodb compass and connect
--- app.js
+++ app.js
@@ -16,6 +16,49 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+app.get('/logout', (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+})
+
+app.post('/login', async (req, res) => {
+  let { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    let user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send("User Not Found");
+    }
+
+    // Compare the provided password with the hashed password in the database
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        console.error("Error comparing passwords:", err);
+        return res.status(500).send("Internal server error");
+      }
+      if (result) {
+        // Passwords match, login successful
+        res.status(200).send("Login Successful");
+      } else {
+        // Passwords don't match, redirect to login page
+        res.redirect('/login');
+      }
+    });
+  } catch (err) {
+    console.error("Error logging in:", err);
+    res.status(500).send("Internal server error");
+  }
+});
+
+
 app.post('/registerUser', async (req, res) => {
   let { username, name, email, password, age } = req.body;
 
@@ -61,6 +104,9 @@ app.post('/registerUser', async (req, res) => {
     });
   });
 });
+
+
+
 
 
 app.listen(3000, () => {
